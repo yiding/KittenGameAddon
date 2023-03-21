e97e22eee01f97b426c270874a7ddd12db5e3a56
Yiding Jia <yiding@dev.i.4gqc.com>
2023-03-21 15:25:49 -0700

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -17,35 +17,36 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-      new CopyPlugin([
-          {
-              from: 'manifest.json',
-          },
-          {
-              from: 'src/fabricconfig.js',
-          },
-          {
-              from: 'page_action_icon.svg',
-          },
-          {
-            from: 'node_modules/@uifabric/icons/fonts/fabric-icons-*.woff',
-            ignore: ['node_modules/@uifabric/icons/fonts/fabric-icons-*-*.woff'],
-            to: 'fonts/[name].[ext]',
-          },
-      ]),
-      new HtmlWebpackPlugin({
-        title: 'Popup',
-        filename: 'popup.html',
-        chunks: ['popup'],
-        template: 'assets/popup.html',
-      }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'manifest.json',
+        },
+        {
+          from: 'src/fabricconfig.js',
+        },
+        {
+          from: 'page_action_icon.svg',
+        },
+        {
+          from: 'node_modules/@uifabric/icons/fonts/fabric-icons-*.woff',
+          to: 'fonts/[name].[ext]',
+        },
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Popup',
+      filename: 'popup.html',
+      chunks: ['popup'],
+      template: 'assets/popup.html',
+    }),
   ],
   devtool: 'inline-source-map',
   mode: 'development'
